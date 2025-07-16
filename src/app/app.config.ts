391a523d31fb32f86892de41ead3cf260a33b07f
waqar95 <waqar95ahmed@gmail.com>
2025-07-16 15:09:46 +0200

    Added direct values replaced with css variable

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,22 +3,9 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 
-import { FormsModule } from '@angular/forms';
-import { AppComponent } from './app.component';
-
-@NgModule({
-  declerations:[
-    AppComponent
-  ],
-  imports:[
-    BrowserModule,
-    FormsModule
-  ],
-  prividers: [],
-  bootstrap: [AppComponent]
-})
-
-
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+  ],
 };
